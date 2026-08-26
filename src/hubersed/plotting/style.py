b0b8ff543cab2c7217d458eb762de75c7332d6d7
Nikhil Garuda <nikhilgaruda@live.com>
2026-08-26 18:03:14 -0500

    make latex optional for plotting

--- src/hubersed/plotting/style.py
+++ src/hubersed/plotting/style.py
@@ -1,5 +1,12 @@
 from importlib.resources import files
+from shutil import which
+
+from matplotlib import rcParams
 from matplotlib import style as _mpl_style
+
 
 def use_apj_style():
     _mpl_style.use(files("hubersed.plotting") / "data" / "apj.mplstyle")
+    
+    if which("latex") is None:
+        rcParams["text.usetex"] = False
