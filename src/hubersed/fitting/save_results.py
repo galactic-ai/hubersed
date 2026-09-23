15a50828eab549b9d3adb55b5bb8827327e399dd
Nikhil Garuda <nikhilgaruda@live.com>
2026-09-23 00:10:49 -0500

    Remove unused save_results module

--- src/hubersed/fitting/save_results.py
+++
@@ -1,9 +0,0 @@
-import os
-import pickle
-
-
-def load_galaxy_results(gal_id, output_dir):
-    """Load full results for one galaxy."""
-    pkl_path = os.path.join(output_dir, str(gal_id), "results.pkl")
-    with open(pkl_path, "rb") as f:
-        return pickle.load(f)
